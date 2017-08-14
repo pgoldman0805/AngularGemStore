3f69265f69ebe2550f0b21f334f377e45001ae98
Paul Goldman <paulgoldmanmusic@gmail.com>
2017-08-14 14:52:55 -0500

    Added conditional classes, panelController for details, specifications, and reviews tabs

--- app.js
+++ app.js
@@ -45,16 +45,23 @@
 
     //Using the "two-argument" call to 'module' defines a module
     // The 2nd argument ([]) allows you to supply a list of other modules that you "inject" into this module. 
-    angular.module("myApp", []);
+    var myApp = angular.module("myApp", []);
 
     // Using the "one-argument" call will fetch a previously defined module from the angular runtime.
-    angular.module("myApp").controller("myController", function ($scope) {
+    myApp.controller("myController", function ($scope) {
         $scope.Model = products;
         //    alert("welcome, Paul!");
     });
 
+    myApp.controller("PanelController", function ($scope) {
+        $scope.tab = 1;
+        $scope.selectTab = function (newTab) {
+            $scope.tab = newTab;
+        };
+    });
+
     // This is a custom directive that I've defined, not something built into Angular. I can stitch the div into my code by putting <my-directive /> into a page, as long as it is a descendant of myApp.
-    angular.module("myApp").directive("myDirective", function () {
+    myApp.directive("myDirective", function () {
         return {
             template: "<div>This is a custom directive</div>"
         };
